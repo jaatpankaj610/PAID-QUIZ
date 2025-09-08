c78522d371afdb12b7850c850cf145d5f5bffd9e
jaatpankaj610 <12jaat13@gmail.com>
2025-09-08 12:16:50 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('rajasthan-gk-quiz-cache-v2').then(cache => {
+        caches.open('rajasthan-gk-quiz-cache-v3').then(cache => {
             return cache.addAll([
                 '/rajasthan-gk-quiz/index.html',
+                '/rajasthan-gk-quiz/quiz.html',
                 '/rajasthan-gk-quiz/',
                 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
                 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap'
