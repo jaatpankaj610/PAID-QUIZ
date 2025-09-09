da19c7afa6628ddea10cc39837a1c451605cd98e
jaatpankaj610 <12jaat13@gmail.com>
2025-09-09 02:08:28 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ self.addEventListener('install', (event) => {
             return cache.addAll([
                 '/PAID-QUIZ/free-quiz.html',
                 '/PAID-QUIZ/index.html',
+                '/PAID-QUIZ/admin.html',
                 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
             ]);
         })
@@ -12,7 +13,10 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
     if (event.request.url.includes('/freequestions.json')) {
-        return fetch(event.request, { cache: 'no-store' }); // JSON को सीधा फेच, कैश न करे
+        return fetch(event.request, { cache: 'no-store', mode: 'cors' }); // JSON को सीधा फेच, कैश न करे
+    }
+    if (event.request.url.includes('/paidquestions.json')) {
+        return fetch(event.request, { cache: 'no-store', mode: 'cors' }); // पेड JSON भी
     }
     event.respondWith(
         caches.match(event.request).then((response) => {
